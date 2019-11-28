a912260b5f744c701199e91cc9bce46e9cbd1567
Relang <miguerojas86@gmail.com>
2019-11-28 16:37:47 -0300

    pagina funcionando sin carrucel

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,6 +1,16 @@
+from django.conf.urls import url
 from django.urls import path
 from . import views
 
+
+
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.Index, name='Index'),
+    path('Registro.html', views.Registro, name='Registro'),
+    path('Ingreso.html', views.Ingreso, name='Ingreso'),
+    path('Animes.html', views.Animes, name='Animes'),
+    path('post_list', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
